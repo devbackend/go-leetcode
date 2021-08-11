bf52c98de2e40a59ea935ea91b0d9a36261fbb93
Ivan Krivonos <devbackend@yandex.ru>
2021-08-11 08:44:13 +0300

    Add solution for power of four

---
+++ pkg/easy/power/four.go
@@ -0,0 +1,21 @@
+package power
+
+func IsPowerOfFour(n int) bool {
+	if n <= 0 {
+		return false
+	}
+
+	if n == 1 {
+		return true
+	}
+
+	for n > 1 {
+		if n%4 != 0 {
+			return false
+		}
+
+		n /= 4
+	}
+
+	return true
+}
