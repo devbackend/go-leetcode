05e412e9f87eda72435c157dee1936f317f605df
Ivan Krivonos <devbackend@yandex.ru>
2021-12-25 20:01:12 +0300

    Add solution for single number by XOR

--- pkg/easy/single_number.go
+++ pkg/easy/single_number.go
@@ -2,19 +2,10 @@ package easy
 
 // SingleNumber for https://leetcode.com/problems/single-number/
 func SingleNumber(nums []int) int {
-	doubles := map[int]struct{}{}
-
 	res := 0
 
 	for _, v := range nums {
-		if _, ok := doubles[v]; !ok {
-			doubles[v] = struct{}{}
-			res += v
-
-			continue
-		}
-
-		res -= v
+		res ^= v
 	}
 
 	return res
